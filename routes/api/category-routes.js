fd98fff28986cea5c45bbb118469d73e3e2e7520
Ricky Palmer <r.palmer@outlook.com>
2022-07-10 21:36:00 +0100

    added category and tag routes, fixed data clearing bug

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  try {
+    const categoryData = await Category.findAll( {
+      include: [{ model: Product }]
+    });
+      res.status(200).json(categoryData);
+      
+      } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id"});
+    }
+
+    res.status(200).json(categoryData);
+
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 router.post('/', (req, res) => {
