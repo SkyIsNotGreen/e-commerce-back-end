40827e07aa3fd287ce03551963e5e7d8714b9506
Ricky Palmer <r.palmer@outlook.com>
2022-07-10 22:01:33 +0100

    added update routes for category, tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,8 +49,26 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+
+  try {
+    const tagData = await Tag.update({tag_name: req.body.tag_name}, {
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if(!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -63,7 +81,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if(!tagData) {
-      res.status(404).json({ message: 'No tag found with this id!' });
+      res.status(404).json({ message: "No tag found with this id!" });
       return;
     }
 
